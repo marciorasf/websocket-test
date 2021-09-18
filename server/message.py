1d90be0ccd9f58dfc551e2019acaf982c5c63789
Marcio Souza Filho <marciorasf@gmail.com>
2021-09-18 12:55:12 -0300

    Add Message.from_json().

--- server/message.py
+++ server/message.py
@@ -1,6 +1,8 @@
 import json
 from dataclasses import dataclass
-from typing import Dict, Literal, Union
+from typing import Dict, Literal, Type, TypeVar, Union
+
+T = TypeVar("T", bound="Message")
 
 
 @dataclass
@@ -10,3 +12,7 @@ class Message:
 
     def to_json(self) -> str:
         return json.dumps({"action": self.action, "payload": self.payload})
+
+    @classmethod
+    def from_json(cls: Type[T], data: Dict) -> T:
+        return Message(action=data["action"], payload=data["payload"])  # type: ignore
--- tests/test_message.py
+++ tests/test_message.py
@@ -1,4 +1,6 @@
 from server.message import Message
+from unittest.test.test_assertions import Test_Assertions
+import json
 
 
 def test_to_json():
@@ -6,3 +8,13 @@ def test_to_json():
 
     json_message = message.to_json()
     assert json_message == '{"action": "subscribe", "payload": {"stream": "all"}}'
+
+
+def test_from_json():
+    data = json.loads('{"action": "subscribe", "payload": {"stream": "all"}}')
+
+    message = Message.from_json(data)
+
+    Test_Assertions().assertIsInstance(message, Message)
+    assert message.action == "subscribe"
+    assert message.payload == {"stream": "all"}
