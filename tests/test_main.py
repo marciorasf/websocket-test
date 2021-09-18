93eb61f0a871634fbf7e9a8acc1930d9435bd61e
Marcio Souza Filho <marciorasf@gmail.com>
2021-09-18 17:02:34 -0300

    Add timeout to server test.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,18 +1,20 @@
+import pytest
 from fastapi import WebSocket
 from fastapi.testclient import TestClient
 from server.main import app
 from server.message import Message
 
 
+@pytest.mark.timeout(1)
 def test_main():
-    with TestClient(app) as client:
-        with client.websocket_connect("/") as ws:
-            ws: WebSocket
+    client = TestClient(app)
+    with client.websocket_connect("/") as ws:
+        ws: WebSocket
 
-            payload = Message(action="subscribe", payload=None).to_json()
-            ws.send_text(payload)
+        payload = Message(action="subscribe", payload=None).to_json()
+        ws.send_text(payload)
 
-            payload = Message(action="unsubscribe", payload=None).to_json()
-            ws.send_text(payload)
+        payload = Message(action="unsubscribe", payload=None).to_json()
+        ws.send_text(payload)
 
-            ws.close()
+        ws.close()
