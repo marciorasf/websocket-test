70f4fad6c8f5eb9fce9bb63517aee22202b9c072
Marcio Souza Filho <marciorasf@gmail.com>
2021-09-18 17:05:40 -0300

    Log only INFO level or above.

--- server/logger.py
+++ server/logger.py
@@ -1,7 +1,7 @@
 import logging
 
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 logging.getLogger("uvicorn.error").setLevel(logging.WARNING)
 
 logger = logging.getLogger("server")
