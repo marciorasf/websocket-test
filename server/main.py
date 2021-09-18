fccd101c9bf80caee780484afd13e24e281511bb
Marcio Souza Filho <marciorasf@gmail.com>
2021-09-18 01:26:29 -0300

    Use async generator and catch error.

--- server/main.py
+++ server/main.py
@@ -1,11 +1,34 @@
+import logging
+
 from fastapi import FastAPI, WebSocket
+from websockets.exceptions import ConnectionClosedOK
+
+from server.number_generator import create_even_generator
 
 app = FastAPI()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("server")
+
+
+number_generator = create_even_generator()
+
+
+@app.on_event("startup")
+async def on_startup():
+    pass
+
+
+@app.on_event("shutdown")
+async def on_shutdown():
+    pass
 
 
 @app.websocket("/")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+    try:
+        async for number in number_generator.numbers():
+            await websocket.send_text(str(number))
+    except ConnectionClosedOK:
+        logger.debug("Closed connection.")
