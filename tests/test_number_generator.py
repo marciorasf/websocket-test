208c4ee49f569c0d35baac5395717adb2beb93f2
Marcio Souza Filho <marciorasf@gmail.com>
2021-09-18 01:19:02 -0300

    Simplify tests.

--- tests/test_number_generator.py
+++ tests/test_number_generator.py
@@ -1,44 +1,33 @@
 import pytest
 from server.number_generator import NumberGenerator, create_even_generator, create_odd_generator
+from asyncstdlib.builtins import anext, aiter
 
 
 @pytest.mark.asyncio
 async def test_default_numbers() -> None:
     generator = NumberGenerator()
+    numbers = aiter(generator.numbers())
 
-    iterations = 0
-    async for number in generator.numbers():
-        iterations += 1
-
-        assert number == iterations - 1
-
-        if iterations > 3:
-            break
+    assert await anext(numbers) == 0
+    assert await anext(numbers) == 1
+    assert await anext(numbers) == 2
 
 
 @pytest.mark.asyncio
 async def test_even_generator() -> None:
     generator = create_even_generator()
+    numbers = aiter(generator.numbers())
 
-    iterations = 0
-    async for number in generator.numbers():
-        iterations += 1
-
-        assert number == (iterations - 1) * 2
-
-        if iterations > 3:
-            break
+    assert await anext(numbers) == 0
+    assert await anext(numbers) == 2
+    assert await anext(numbers) == 4
 
 
 @pytest.mark.asyncio
 async def test_odd_generator() -> None:
     generator = create_odd_generator()
+    numbers = aiter(generator.numbers())
 
-    iterations = 0
-    async for number in generator.numbers():
-        iterations += 1
-
-        assert number == (iterations - 1) * 2 + 1
-
-        if iterations > 3:
-            break
+    assert await anext(numbers) == 1
+    assert await anext(numbers) == 3
+    assert await anext(numbers) == 5
