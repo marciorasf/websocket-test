3c1079021e2d26cd5daac6b1ec6c1d680dcb9a26
Marcio Souza Filho <marciorasf@gmail.com>
2021-09-27 18:49:03 -0300

    Add test.

--- tests/test_number_generator.py
+++ tests/test_number_generator.py
@@ -1,11 +1,26 @@
 import pytest
-from server.number_generator import NumberGenerator, create_even_generator, create_odd_generator
-from asyncstdlib.builtins import anext, aiter
+from asyncstdlib.builtins import aiter, anext
+from server.number_generator import (
+    NumberGenerator,
+    create_default_generator,
+    create_even_generator,
+    create_odd_generator,
+)
 
 
 @pytest.mark.asyncio
-async def test_default_numbers() -> None:
+async def test_default_number_generator() -> None:
     generator = NumberGenerator(interval_in_seconds=0)
+    numbers = aiter(generator.numbers())
+
+    assert await anext(numbers) == 0
+    assert await anext(numbers) == 1
+    assert await anext(numbers) == 2
+
+
+@pytest.mark.asyncio
+async def test_default_generator() -> None:
+    generator = create_default_generator(interval_in_seconds=0)
     numbers = aiter(generator.numbers())
 
     assert await anext(numbers) == 0
