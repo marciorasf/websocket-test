889f68a9e0ce9725c820903c625926b6af2419db
Marcio Souza Filho <marciorasf@gmail.com>
2021-09-27 19:21:16 -0300

    Fix delivery_manager test.

--- tests/test_delivery_manager.py
+++ tests/test_delivery_manager.py
@@ -1,11 +1,14 @@
 import asyncio
+import json
+from datetime import datetime
 from typing import List
 from unittest.mock import Mock
 
 import pytest
 from fastapi import WebSocket
+from freezegun import freeze_time
 from server.client_manager import Client, ClientManager
-from server.delivery_manager import DeliveryManager
+from server.delivery_manager import DeliveryManager, Message
 from server.number_generator import NumberGenerator
 
 
@@ -13,10 +16,16 @@ class MockClient(Client):
     def __init__(self, id: str, ws: WebSocket) -> None:
         self.id = id
         self.ws = ws
-        self.messages: List[str] = []
+        self.messages: List[Message] = []
 
     async def send_message(self, message: str) -> None:
-        self.messages.append(message)
+        parsed_message = json.loads(message)
+        self.messages.append(
+            Message(
+                content=parsed_message["content"],
+                timestamp=datetime.fromisoformat(parsed_message["timestamp"]),
+            )
+        )
 
 
 @pytest.fixture
@@ -24,7 +33,8 @@ def ws() -> WebSocket:
     return Mock(spec=WebSocket)
 
 
-@pytest.mark.skip
+# @pytest.mark.skip
+@freeze_time("2021-09-27 21:00:00", tick=True)
 @pytest.mark.asyncio
 async def test_delivery_manager(ws: Mock) -> None:
     client = MockClient("1", ws)
@@ -40,5 +50,15 @@ async def test_delivery_manager(ws: Mock) -> None:
 
     task.cancel()
 
-    assert 0 in client.messages
-    assert 1 in client.messages
+    assert len(client.messages) >= 2
+
+    min_time = datetime.fromisoformat("2021-09-27 21:00:00")
+    max_time = datetime.fromisoformat("2021-09-27 21:00:01")
+
+    msg = client.messages.pop(0)
+    assert msg.content == 0
+    assert min_time < msg.timestamp < max_time
+
+    msg = client.messages.pop(0)
+    assert msg.content == 1
+    assert min_time < msg.timestamp < max_time
