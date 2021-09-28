06d32de5f23a9f05c049b5e37ab49152ca2eeeb3
Marcio Souza Filho <marciorasf@gmail.com>
2021-09-27 21:27:02 -0300

    Add tests to main.

---
+++ tests/test_main.py
@@ -0,0 +1,16 @@
+from fastapi.testclient import TestClient
+from server.main import app, manager
+
+
+def test_get_root() -> None:
+    with TestClient(app) as client:
+        response = client.get("/")
+        assert response.status_code == 200
+
+
+def test_setup_and_teardown() -> None:
+    with TestClient(app):
+        assert not manager.delivery_task.done()
+
+    assert manager.delivery_task.done()
+    assert manager.delivery_task.cancelled()
