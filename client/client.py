3be98dae97788977545665dc8f4fc226803063ac
Marcio Souza Filho <marciorasf@gmail.com>
2021-10-02 11:46:42 -0300

    Improve client code.

--- client/client.py
+++ client/client.py
@@ -1,15 +1,19 @@
-import websocket
 import json
+from typing import Any, List
 
-ws = websocket.WebSocket()
-ws.connect("ws://localhost:8000")
-ws.send(json.dumps(dict(action="subscribe", payload="")))
+import websocket
 
-responses = []
-for _ in range(5):
-    responses.append(ws.recv())
 
-print(responses)
+async def simulate_client(n_messages: int) -> List[Any]:
+    ws = websocket.WebSocket()
+    ws.connect("ws://localhost:8000")
+    ws.send(json.dumps(dict(action="subscribe", payload="")))
 
-ws.send(json.dumps(dict(action="unsubscribe", payload="")))
-ws.close()
+    responses = []
+    for _ in range(n_messages):
+        responses.append(ws.recv())
+
+    ws.send(json.dumps(dict(action="unsubscribe", payload="")))
+    ws.close()
+
+    return responses
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,3 @@
-import pytest
 from fastapi.testclient import TestClient
 from server.main import app, manager
 
@@ -9,20 +8,9 @@ def test_get_root() -> None:
         assert response.status_code == 200
 
 
-# TODO Make this test work.
-# Currently, it halts.
-@pytest.mark.skip
-@pytest.mark.asyncio
-def test_websocket_endpoint() -> None:
-    with TestClient(app) as client:
-        with client.websocket_connect("/") as ws:
-            ws.send_json('{"action": "subscribe", "payload": ""}')
-            ws.receive_json()
-            assert True
-
-
 def test_setup_and_teardown() -> None:
     with TestClient(app):
+        assert manager.delivery_task is not None
         assert not manager.delivery_task.done()
 
     assert manager.delivery_task.done()
