2410c2cca635ec4fa83c64a8d0e48f281a7d686c
Marcio Souza Filho <marciorasf@gmail.com>
2021-10-04 20:03:11 -0300

    Change attributes to private.

--- server/delivery_manager.py
+++ server/delivery_manager.py
@@ -11,9 +11,9 @@ from server.response import Response
 
 class DeliveryManager:
     def __init__(self, generators: List[NumberGenerator], client_manager: ClientManager):
-        self.generators = generators
-        self.client_manager = client_manager
-        self.message_queue: Queue[str] = Queue()
+        self._generators = generators
+        self._client_manager = client_manager
+        self._message_queue: Queue[str] = Queue()
         self._tasks: List[Task[None]] = []
 
     def deliver_messages(self) -> None:
@@ -26,21 +26,21 @@ class DeliveryManager:
     async def _agreggate_messages(self) -> None:
         tasks = map(
             lambda generator: asyncio.create_task(self._enqueue_generator_messages(generator)),
-            self.generators,
+            self._generators,
         )
         self._tasks.extend(tasks)
 
     async def _enqueue_generator_messages(self, generator: NumberGenerator) -> None:
         async for number in generator.numbers():
-            await self.message_queue.put(
+            await self._message_queue.put(
                 Response(content=number, timestamp=datetime.now()).to_json()
             )
 
     async def _send_messages(self) -> None:
         while True:
-            if not self.message_queue.empty():
-                message = await self.message_queue.get()
-                tasks = [client.send_message(message) for client in self.client_manager.clients()]
+            if not self._message_queue.empty():
+                message = await self._message_queue.get()
+                tasks = [client.send_message(message) for client in self._client_manager.clients()]
                 await asyncio.gather(*tasks)
             else:
                 await asyncio.sleep(0.01)
