deba0d5657a7085f4ce92d2819c3097db973f0ab
Marcio Souza Filho <marciorasf@gmail.com>
2021-10-04 20:03:40 -0300

    Change attributes to private.

--- server/request_handler.py
+++ server/request_handler.py
@@ -5,7 +5,7 @@ from server.request import Request
 
 class RequestHandler:
     def __init__(self, client_manager: ClientManager) -> None:
-        self.client_manager = client_manager
+        self._client_manager = client_manager
 
     def handle(self, client: Client, request: Request) -> None:
         if request.action == "subscribe":
@@ -18,9 +18,9 @@ class RequestHandler:
     def _subscribe(self, client: Client) -> None:
         logger.debug(f"Subscribing client '{client.id}'.")
 
-        self.client_manager.add(client)
+        self._client_manager.add(client)
 
     def _unsubscribe(self, client: Client) -> None:
         logger.debug(f"Unsubscribing client '{client.id}'.")
 
-        self.client_manager.remove(client.id)
+        self._client_manager.remove(client.id)
