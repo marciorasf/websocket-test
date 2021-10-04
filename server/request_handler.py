a089fb42d4898e648d8bb7408cdbcd4738fdb34e
Marcio Souza Filho <marciorasf@gmail.com>
2021-10-04 20:59:37 -0300

    Add subscribed stream to client state.

--- server/request_handler.py
+++ server/request_handler.py
@@ -9,18 +9,20 @@ class RequestHandler:
 
     def handle(self, client: Client, request: Request) -> None:
         if request.action == "subscribe":
-            self._subscribe(client)
+            self._subscribe(client, request.stream)
         elif request.action == "unsubscribe":
-            self._unsubscribe(client)
+            self._unsubscribe(client, request.stream)
         else:
             raise ValueError("Unknown action.")
 
-    def _subscribe(self, client: Client) -> None:
+    def _subscribe(self, client: Client, stream: str) -> None:
         logger.debug(f"Subscribing client '{client.id}'.")
 
+        client.subscribe(stream)
         self._client_manager.add(client)
 
-    def _unsubscribe(self, client: Client) -> None:
+    def _unsubscribe(self, client: Client, stream: str) -> None:
         logger.debug(f"Unsubscribing client '{client.id}'.")
 
+        client.unsubscribe(stream)
         self._client_manager.remove(client.id)
