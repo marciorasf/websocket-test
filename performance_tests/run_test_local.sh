f2a0ecab302e3b835885314ba071d21cab642fd1
Marcio Souza Filho <marciorasf@gmail.com>
2021-10-19 20:29:57 -0300

    Fix script that runs test.

--- performance_tests/run_test_local.sh
+++ performance_tests/run_test_local.sh
@@ -3,7 +3,7 @@ TEST_NAME=$1
 yarn build
 
 docker run \
-  --network performance_test_default \
+  --network performance_tests_default \
   --volume $PWD/build:/tests \
   -i loadimpact/k6 \
   run /tests/$TEST_NAME.js
