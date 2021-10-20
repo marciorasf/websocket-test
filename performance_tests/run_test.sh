5314744c4b0a2d2e9c0380da34ffd34052e22908
Marcio Souza Filho <marciorasf@gmail.com>
2021-10-19 21:37:39 -0300

    Improve script to run performance tests.

--- performance_tests/run_test.sh
+++ performance_tests/run_test.sh
@@ -3,7 +3,9 @@ TEST_NAME=$1
 yarn build
 
 docker run \
+  -- user 0 \
   --network performance_tests_default \
   --volume $PWD/build:/tests \
+  --volume $PWD/output:/output \
   -i loadimpact/k6 \
-  run /tests/$TEST_NAME.js
+  run --summary-export=/output/result.json /tests/$TEST_NAME.js
