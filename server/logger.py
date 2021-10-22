1d1c0ed22a04a57a2d1642dada9362c2920dedba
Marcio Souza Filho <marciorasf@gmail.com>
2021-10-22 19:10:45 -0300

    Add coloredlogs.

--- server/logger.py
+++ server/logger.py
@@ -1,5 +1,8 @@
 import logging
 
+import coloredlogs
 
-logging.basicConfig(level=logging.INFO)
+coloredlogs.install(level=logging.INFO)
 logger = logging.getLogger("server")
+
+logging.getLogger("uvicorn.error").setLevel(logging.WARNING)
